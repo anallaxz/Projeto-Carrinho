1ea09c11371080fe8ef41f283ea44c159ab519eb
anallaxz <lunna.devv@gmail.com>
2025-08-23 23:24:59 -0300

    update3

--- script.js
+++ script.js
@@ -2,7 +2,20 @@
 const demoModal = document.getElementById("demoModal");
 const openDemo = document.getElementById("openDemo");
 const closeDemo = document.getElementById("closeDemo");
+const demoVideo = document.getElementById("demoVideo");
 
-openDemo.addEventListener("click", () => demoModal.classList.add("open"));
-closeDemo.addEventListener("click", () => demoModal.classList.remove("open"));
-window.addEventListener("click", e => { if(e.target === demoModal) demoModal.classList.remove("open") });
+openDemo.addEventListener("click", () => {
+  demoModal.classList.add("open");
+  demoVideo.play();
+});
+
+function closeDemoModal() {
+  demoModal.classList.remove("open");
+  demoVideo.pause();
+  demoVideo.currentTime = 0;
+}
+
+closeDemo.addEventListener("click", closeDemoModal);
+window.addEventListener("click", e => {
+  if(e.target === demoModal) closeDemoModal();
+});
